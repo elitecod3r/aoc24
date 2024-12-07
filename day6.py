2683e3ced05ced9122b35a99fc9ef21206efb834
elitecod3r <chinugeetu@yahoo.com>
2024-12-06 18:46:36 -0800

    I tried to take some shortcut but I had to make a deep copy and recursive call ultimately. That works for the small sample but is taking a long time for the big test-case. I need to re-think the approach.

--- day6.py
+++ day6.py
@@ -1,6 +1,4 @@
-from collections import Counter
-import sys
-
+import copy
 
 # read until end of file
 def read_file(filename):
@@ -15,7 +13,6 @@ def read_file(filename):
         '<': 'left',
         '>': 'right'
     }
-
 
     with open(filename, 'r') as f:
         # read one line at a time 
@@ -60,57 +57,110 @@ def test_read_file():
     assert pdir == 'up'
 
 def print_grid(grid):
-    for row in grid:
-        string = ''.join(map(str, row))
-        print(string)
+    print('   ', end='')
+    for col_idx in range(len(grid[0])):
+        print(f'{col_idx: >2}', end='')
+    print('')
 
-def move_player(grid, prow, pcol, pdir):
-    count = 1 
-    grid[prow][pcol] = 2
+    for row_idx, row in enumerate(grid):
+        string = ''.join([f"{num: >2}" for num in row])
+        print(f'{row_idx:>2} {string}')
 
-    while True:
-        if pdir == 'up':
-            prow -= 1
-        elif pdir == 'down':
-            prow += 1  
-        elif pdir == 'left':
-            pcol -= 1
-        elif pdir == 'right':   
-            pcol += 1
+def move_player(grid, prow, pcol, pdir, obstacle_added=0):
+    # Returns (number_of_cells_visited, )
+    # Returns distinct_positions, obstacle_count, loop_detected 
+    #  distinct_positions: number of cells visited by the player until they 
+    #       walk off the grid. 
+    #  obstacle_count: number of obstacles we can place on the route to 
+    #       place the user on the loop.
+    #  loop_detected: if there is a loop without adding an obstacle. This is 
+    #       used to compute obstacle count since initial grid is not expected
+    #       have loop
 
-        
-        if prow < 0 or prow >= len(grid) or pcol < 0 or pcol >= len(grid[0]):
+    if obstacle_added > 1:
+        # We can only place one obstacle. Return if we have alredy placed 
+        #  2 or more obstacles. 
+        return 0, 0, False     
+
+    # bit mask for steps in different direction
+    dir_map = { "up": 2, "down": 4, "left": 8, "right": 16 }
+    # right_turn gives the next direciton if player takes a right turn
+    right_turn = {"up": "right", "right": "down", "down": "left", "left": "up"}
+    # next_setp gives the increments for row and col if player takes one more step
+    next_step = {"up": (-1, 0), "down": (+1, 0), "right": (0, +1), "left": (0, -1)}
+    
+    obstacle_count = 0    
+    distinct_positions = 1   # initial position is counted as already visited
+    grid[prow][pcol] = grid[prow][pcol] | dir_map[pdir]
+    loop_detected = False 
+
+    while not loop_detected:
+        if False and obstacle_added == 0:  # don't print in recursive calls
+            print('\n')
+            print(f'== Player is at {prow}, {pcol} facing {pdir} ==')
+            print_grid(grid)
+
+        # next_row, next_col are where we will move to next if there is no obstacle in front
+        # next_dir is where we will turn if there is an obstacle in front
+        row_incr, col_incr = next_step[pdir]
+        next_row = prow + row_incr
+        next_col = pcol + col_incr  
+        next_dir = right_turn[pdir]
+
+        # We are done if the player steps off the grid. 
+        if next_row < 0 or next_row >= len(grid) or next_col < 0 or next_col >= len(grid[0]):
             break
 
-        if grid[prow][pcol] == 1:  
-            # we hit a wall. First back off the move operation and then turn right
-            if pdir == 'up':
-                prow += 1
-                pdir = 'right'
-            elif pdir == 'down':
-                prow -= 1  
-                pdir = 'left'
-            elif pdir == 'left':
-                pcol += 1
-                pdir = 'up'
-            elif pdir == 'right':   
-                pcol -= 1
-                pdir = 'down'
-            #print(f'Turned {pdir} at {prow},{pcol}  count = {count}')
-            #print_grid(grid)
+        # if we have an obstacle in front, we will take a turn and continue on our path
+        if grid[next_row][next_col] == 1:  
+            # When we take a turn, we stay in current location but change direction
+            pdir = next_dir
+            if grid[prow][pcol] & dir_map[pdir]:
+                loop_detected = True
+            else:
+                grid[prow][pcol] = grid[prow][pcol] | dir_map[pdir] 
+            continue 
+
+        # At this stage, the cell in front is empty. We will first check 
+        # if we get a loop if we place an obstacle on the next step. 
+        if True:
+            new_grid = copy.deepcopy(grid)
+            new_grid[next_row][next_col] = 1
+            dp, oc, new_loop_detected = move_player(new_grid, prow, pcol, pdir, obstacle_added + 1)
+            if new_loop_detected:
+                obstacle_count += 1
+                #print(f'** found loop at {next_row}, {next_col} **')
+        
+        # We will now move to the next cell. Advance row/col but pdir stays the same
+        prow = next_row
+        pcol = next_col
 
         if grid[prow][pcol] == 0:
-            grid[prow][pcol] = 2
-            count += 1
+            distinct_positions += 1
+            print(distinct_positions)
 
-    return count
-    
+        if grid[prow][pcol] & dir_map[pdir]:
+            loop_detected = True
+        else:
+            grid[prow][pcol] = grid[prow][pcol] | dir_map[pdir] 
+
+    return distinct_positions, obstacle_count, loop_detected
+
+def test_move_player():
+    distinct_positions, obstacle_count, loop_detected = move_player(*read_file("day6_sample.txt"))
+    assert distinct_positions == 41
+    assert obstacle_count == 6
+    assert loop_detected == False
+
+    distinct_positions, obstacle_count, loop_detected = move_player(*read_file("day6_input.txt"))
+    assert distinct_positions == 858
+    assert obstacle_count == 0
+    assert loop_detected == False
+
 
 if __name__ == '__main__':
-    count = move_player(*read_file("day6_sample.txt"))
-    print(count)
+    print(move_player(*read_file("day6_sample.txt")))
+    print(move_player(*read_file("day6_input.txt")))
 
-    count = move_player(*read_file("day6_input.txt"))
-    print(count)
 
 
