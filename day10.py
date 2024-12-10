4b62c6f23018570fe80bde75a90760e3e29a7abd
elitecod3r <chinugeetu@yahoo.com>
2024-12-09 23:20:47 -0800

    Part2 is actually easier than part1 :-)

--- day10.py
+++ day10.py
@@ -52,6 +52,46 @@ def compute_score(grid):
         score += len(trail_set)
     return score
 
+def next_step_part2(grid, cell_dict, num):
+    new_cell_dict = {}
+    for cell, count in cell_dict.items():
+        row, col = cell
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if abs(i) == abs(j):
+                    continue
+                new_row = row + i
+                new_col = col + j
+                if new_row < 0 or new_row >= len(grid):
+                    continue
+                if new_col < 0 or new_col >= len(grid[0]):
+                    continue
+                if grid[new_row][new_col] == num:
+                    new_cell = (new_row, new_col)
+                    if new_cell not in new_cell_dict:
+                        new_cell_dict[new_cell] = 0
+                    new_cell_dict[new_cell] += count
+    return new_cell_dict
+
+def compute_score_part2(grid):
+    cell_dict = {}
+    for row_idx, row in enumerate(grid):
+        for col_idx, cell in enumerate(row):
+            if cell == 9:
+                cell_key = (row_idx, col_idx)
+                cell_dict[cell_key] = 1
+
+    for next_num in range(8, -1, -1):
+        #print(cell_dict)
+        next_dict = next_step_part2(grid, cell_dict, next_num)
+        cell_dict = next_dict
+    
+    #print(cell_dict)
+    score = 0
+    for _, count in cell_dict.items():
+        score += count
+    return score
+
 
 def test_read_file():
     grid = read_file('day10_sample.txt')
@@ -62,9 +102,14 @@ def test_read_file():
 
 def test_compute_score():
     assert compute_score(read_file('day10_sample.txt')) == 36
+    assert compute_score_part2(read_file('day10_sample.txt')) == 81
 
 
 if __name__ == '__main__':
-    test_read_file()
-    print(compute_score(read_file('day10_sample.txt')))
-    print(compute_score(read_file('day10_input.txt')))
+    print("Part1")
+    print('  Sample :', compute_score(read_file('day10_sample.txt')))
+    print('  input  :', compute_score(read_file('day10_input.txt')))
+    print('')
+    print("Part2")
+    print('  Sample :', compute_score_part2(read_file('day10_sample.txt')))
+    print('  input  :', compute_score_part2(read_file('day10_input.txt')))
