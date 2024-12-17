df7280c2de94997efb5a7106a4ad76344d10de13
elitecod3r <chinugeetu@yahoo.com>
2024-12-16 22:39:56 -0800

    Changing to iterative was easy but I still got the same wrong answer 133588. It took a bit of thinking to figure out that we need to keep track of the direction when thinking of the max score. If you enter a cell with a slightly higher score, you may still get to a lower score if the player is already pointing in the right direction.

--- day16.py
+++ day16.py
@@ -1,14 +1,8 @@
-import re
-from math import gcd
-import sys
-import resource
-
 # Values for the grid 
 START = 'S'
 END = 'E'
 WALL = '#'
 EMPTY = '.'
-
 
 def read_file(filename):
     grid = []
@@ -25,35 +19,6 @@ def print_grid(grid):
             print(cell, end='')
     print()
 
-def recurse_path(grid, row, col, direction, score):
-    # Entering the 'curr' cell with direction 'direction' and score 'score'
-    #print(row, col, direction, score, grid[row][col], end='')
-    if grid[row][col] == WALL:
-        #print(' hit a wall')
-        return
-    if grid[row][col] < score:   # new path is longer. return
-        #print(' lower score ')
-        return
-    #print(' processing ')
-    grid[row][col] = score
-
-    if direction in ['N', 'S']:
-        if direction == 'N':
-            recurse_path(grid, row-1, col, 'N', score + 1)
-        else:
-            recurse_path(grid, row+1, col, 'S', score + 1)
-        recurse_path(grid, row, col+1, 'E', score + 1001)
-        recurse_path(grid, row, col-1, 'W', score + 1001)
-    if direction in ['E', 'W']:
-        if direction == 'E':
-            recurse_path(grid, row, col+1, 'E', score + 1)
-        else:
-            recurse_path(grid, row, col-1, 'W', score + 1)
-        recurse_path(grid, row+1, col, 'S', score + 1001)
-        recurse_path(grid, row-1, col, 'N', score + 1001)
-
-    return
-
 def find_path(grid):
     # make a deep copy of the grid
     grid = [row[:] for row in grid]
@@ -61,26 +26,46 @@ def find_path(grid):
     # Prepare the grid 
     for row_index, row in enumerate(grid):
         for col_index, cell in enumerate(row):
+            if cell == WALL:
+                continue
             if cell == START:
                 start_row, start_col = row_index, col_index
-                grid[row_index][col_index] = 0
-            elif cell == END:
+                grid[row_index][col_index] = {'N': 0, 'S': 0, 'E': 0, 'W': 0}
+                continue
+            if cell == END:
                 end_row, end_col = row_index, col_index
-                grid[row_index][col_index] = 1000000000000
-            elif cell == EMPTY:
-                grid[row_index][col_index] = 1000000000000
+            grid[row_index][col_index] = {'N':1000000000000, 'S':1000000000000, 'E':1000000000000, 'W':1000000000000}
 
-    recurse_path(grid, start_row, start_col, 'E', 0)
-    return grid[end_row][end_col]
+    cell_stack = [(start_row, start_col, 'E', 0)]
+    while cell_stack:
+        row, col, direction, score = cell_stack.pop()
+        if grid[row][col] == WALL:
+            continue
+        if grid[row][col][direction] < score:
+            continue
+        grid[row][col][direction] = score
+        if direction in ['N', 'S']:
+            cell_stack.append((row, col+1, 'E', score + 1001))
+            cell_stack.append((row, col-1, 'W', score + 1001))
+            if direction == 'N':
+                cell_stack.append((row-1, col, 'N', score + 1))
+            else:
+                cell_stack.append((row+1, col, 'S', score + 1))
+        if direction in ['E', 'W']:
+            cell_stack.append((row+1, col, 'S', score + 1001))
+            cell_stack.append((row-1, col, 'N', score + 1001))
+            if direction == 'E':
+                cell_stack.append((row, col+1, 'E', score + 1))
+            else:
+                cell_stack.append((row, col-1, 'W', score + 1))
 
+    return min(grid[end_row][end_col].values())
 
+def test_find_path():
+    assert find_path(read_file('day16_sample1.txt')) == 7036
+    assert find_path(read_file('day16_sample2.txt')) == 11048
 
 if __name__ == '__main__':
-    #resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
-    sys.setrecursionlimit(10**6)
-
     print('Part 1')
-    print(find_path(read_file('day16_sample1.txt')))
-    print(find_path(read_file('day16_sample2.txt')))
     print(find_path(read_file('day16_input.txt')))
     # Got answer 133588 by incrasing recursion limit but the answer is too big. 
