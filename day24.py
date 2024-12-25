aaf81b6f12b01eedfc2ed9400348989689f20c13
elitecod3r <chinugeetu@yahoo.com>
2024-12-24 22:11:41 -0800

    WIP - trying to print out in a way that shows the wrong operations more clearly. I may give this a try later when I have some more time.

--- day24.py
+++ day24.py
@@ -94,6 +94,107 @@ def count_true_zvars(var_value_dict):
             #print(var, value, bit_pos, bit_val)
     return zval
 
+# an attempt to draw the graph. Not very useful even for the simpler sample dataset.
+def draw_graph():
+    import networkx as nx
+    import matplotlib.pyplot as plt
+
+    var_value_dict, var_mapping_dict, operator_list = read_file('day24_sample.txt')
+    G = nx.DiGraph()
+    for op, left_var, left_val, right_var, right_val, output_var in operator_list:
+        G.add_edge(left_var, output_var)
+        G.add_edge(right_var, output_var)
+    nx.draw(G, with_labels=True)
+    plt.show()
+
+def replace_vars(op_list, var_change_mapping):
+    new_op_list = []
+    for op, left_var, right_var, output_var in op_list:
+        if left_var in var_change_mapping:
+            left_var = var_change_mapping[left_var]
+        if right_var in var_change_mapping:
+            right_var = var_change_mapping[right_var]
+        if left_var > right_var:
+            left_var, right_var = right_var, left_var
+        if output_var in var_change_mapping:
+            output_var = var_change_mapping[output_var]
+        new_op_list.append((op, left_var, right_var, output_var))
+    return new_op_list
+
+def print_output_order(op_list):
+    output_var_mapper = {}
+    max_z = 0
+    for idx, (op, left_var, right_var, output_var) in enumerate(op_list):
+        output_var_mapper[output_var] = idx
+        if output_var.startswith('z'):
+            z_val = int(output_var[1:])
+            max_z = max(max_z, z_val)
+    
+    for i in range(max_z+1):
+        for letter in ['a', 'b', 'z', 'd', 'c']:
+            output_var = f'{letter}{i:02}'
+            if output_var in output_var_mapper:
+                idx = output_var_mapper[output_var]
+                op, left_var, right_var, output_var = op_list[idx]
+                print(f'{left_var} {op:.3s} {right_var} -> {output_var}')
+
+    print(op_list)
+
+def clean_operators(filename):
+    var_value_dict, var_mapping_dict, operator_list = read_file(filename)
+    # A typical operation set will have the following flows
+    # 
+    # x03 AND y03 -> a01     # change output var to a01
+    # x03 XOR y03 -> b01     # change output var to b01
+    # b01 XOR c00 -> z01     # don't change z var
+    # b01 AND c00 -> d01     # change output var to d01
+    # a01 OR d01 -> c01      # change carry var to c01. This is carry for next round
+    #
+    new_operator_list = []
+    var_change_mapping = {}   
+    for op, left_var, left_val, right_var, right_val, output_var in operator_list:
+        if left_var > right_var:
+            left_var, right_var = right_var, left_var
+        new_operator_list.append((op, left_var, right_var, output_var))
+        
+        #'''
+    for op, left_var, right_var, output_var in new_operator_list:
+        if output_var.startswith('z'):
+            continue
+        if left_var == 'x00':
+            new_output = 'c00'
+            var_change_mapping[output_var] = new_output
+            continue
+            
+        if left_var.startswith('x') and right_var.startswith('y'):
+            var_suffix = left_var[1:]
+            assert var_suffix == right_var[1:], f'Variable suffix mismatch {left_var} {right_var}'
+            if op == 'AND':
+                new_output = f'a{var_suffix}'
+                var_change_mapping[output_var] = new_output
+            elif op == 'XOR':
+                new_output = f'b{var_suffix}'
+                var_change_mapping[output_var] = new_output
+    new_operator_list = replace_vars(new_operator_list, var_change_mapping)
+
+    for op, left_var, right_var, output_var in new_operator_list:
+        if output_var.startswith('z') or left_var.startswith('x'):
+            continue
+        if left_var.startswith('a') or left_var.startswith('b'):
+            var_suffix = left_var[1:]
+        else:
+            print('Unexpected left var', left_var, right_var, output_var)
+
+        if op == 'AND':
+            new_output = f'd{var_suffix[1:]}'
+            var_change_mapping[output_var] = new_output
+        elif op == 'OR':
+            new_output = f'c{var_suffix[1:]}'
+            var_change_mapping[output_var] = new_output
+    new_operator_list = replace_vars(new_operator_list, var_change_mapping)
+    #'''
+    print_output_order(new_operator_list)
+
 
 if __name__ == '__main__':    
     print('Part 1')
@@ -102,3 +203,7 @@ if __name__ == '__main__':
     new_dict = solve_all_values(*read_file('day24_input.txt'))
     print('Input  = ',count_true_zvars(new_dict))
     
+    print('Part 2')
+    #draw_graph(operator_list)
+    #print_neatly('day24_input.txt')
+    clean_operators('day24_input.txt')
